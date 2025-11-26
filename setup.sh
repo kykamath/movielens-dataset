036d63d4d48ab78810e367afe3ed8c543f911b27
kkamath <kkamath@pinterest.com>
2025-11-26 12:04:41 -0800

    modified files.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip install -r requirements.txt
-#wget https://files.grouplens.org/datasets/movielens/ml-32m.zip
-#unzip ml-32m.zip
+wget https://files.grouplens.org/datasets/movielens/ml-32m.zip
+unzip ml-32m.zip
